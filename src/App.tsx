936c59a5c7d431aa2b955dfa2644a8c4f2eb1896
Lucas de Souza <lucaszsep@gmail.com>
2022-08-26 13:43:01 -0300

    Adicionado footer, navbar, home, primeira estilização criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 
 function App() {
   return (
       <>
-      <Home></Home>
+      <Navbar />
+      <Home />
+      <Footer />
       </>
   );
 }
